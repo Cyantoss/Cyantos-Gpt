478481fac335bece8edd1946276fc65ab4ae1a35
Cyantos <115422850+Snahple@users.noreply.github.com>
2023-04-04 17:47:06 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv'
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
-import fs from 'fs';
 
 dotenv.config();
 
@@ -16,39 +15,15 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-const historyFilePath = './history.json';
-
-function readHistory() {
-    try {
-        const historyData = fs.readFileSync(historyFilePath, 'utf8');
-        return JSON.parse(historyData);
-    } catch (error) {
-        console.error(error);
-        return {};
-    }
-}
-
-function writeHistory(history) {
-    fs.writeFileSync(historyFilePath, JSON.stringify(history), 'utf8');
-}
-
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello Nigga'
+        message: 'Hello World'
     })
 });
 
 app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
-        const history = readHistory();
-
-        // Add the user's input to the conversation history
-        if (!history[prompt]) {
-            history[prompt] = [];
-        }
-        history[prompt].push(req.body.userInput);
-        writeHistory(history);
 
         const response = await openai.createCompletion({
             model: "text-davinci-003",
@@ -59,10 +34,6 @@ app.post('/', async (req, res) => {
             frequency_penalty: 0.5,
             presence_penalty: 0,
         });
-
-        // Add the bot's response to the conversation history
-        history[prompt].push(response.data.choices[0].text);
-        writeHistory(history);
 
         res.status(200).send({
             bot: response.data.choices[0].text
